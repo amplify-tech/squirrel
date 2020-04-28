37f9d6337842300f19228b6faae564fee81a5219
akram <akram.iitbombay@gmail.com>
2020-04-28 14:25:45 +0530

    all del

--- squirrel/public/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-from django.contrib.auth.admin import User
-from datetime import datetime
--- squirrel/public/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.public_home, name='public_home'),
-    path('<user_id>', views.thatperson, name='thatperson'),
-    path('search/', views.search, name='search'),
-]
